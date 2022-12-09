b8a6037aadbc3f04cb45e29d6a1120ce528f2d1c
KonLew <surangrat231@gmail.com>
2022-12-09 15:31:58 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    
+    int N;
+    int i=0,E=0,O=0;
+    
+    while (i == 0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        if (N!=0){
+            if (N%2==0){
+                E++;
+            }else{
+                O++;
+            }
+        }
+        if (N == 0){
+            i++;
+        }
+    }
+            
+    
     cout << "#Even numbers = ";
+    cout << E;
+    cout << "\n";
     cout << "#Odd numbers = ";
+    cout << O;
     return 0;
 }
